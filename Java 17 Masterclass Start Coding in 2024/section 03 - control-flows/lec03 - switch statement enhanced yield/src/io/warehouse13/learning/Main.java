6797008a207a17af4a7ca52b81272a274aecfc80
davidvera13 <david.vera@9online.fr>
2024-02-25 19:48:30 +0100

    feat(java 17 master class): section 03 - lec03 - switch statement enhanced yield

---
+++ Java 17 Masterclass Start Coding in 2024/section 03 - control-flows/lec03 - switch statement enhanced yield/src/io/warehouse13/learning/Main.java
@@ -0,0 +1,31 @@
+package io.warehouse13.learning;
+
+public class Main {
+    public static void main(String[] args) {
+        String month;
+
+        month = "XYZ";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+
+        month = "MAY";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+
+
+    }
+
+    public static String getQuarter(String month) {
+
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> { yield "1st"; }
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> {
+                String badResponse = month + " is bad";
+                yield badResponse;
+            }
+        };
+
+    }
+
+}
