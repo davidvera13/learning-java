e6737d1751b35d8988240c97abde0515d127b0e0
davidvera13 <david.vera@9online.fr>
2024-02-25 19:45:06 +0100

    feat(java 17 master class): section 02 - lec01 - expressions

---
+++ Java 17 Masterclass Start Coding in 2024/section 02 - expressions-statements-codeblock-methods/lec01 - expressions/src/io/warehouse13/learning/Main.java
@@ -0,0 +1,16 @@
+package io.warehouse13.learning;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1 mile = 1.609344 km
+        // km = (100 * 1.609344) is an expression
+        double km = (100 * 1.609344);
+        System.out.println(km);
+
+        int highScore = 50;
+        if(highScore >= 50) {
+            // printing an expression
+            System.out.println("Print an expression");
+        }
+    }
+}
