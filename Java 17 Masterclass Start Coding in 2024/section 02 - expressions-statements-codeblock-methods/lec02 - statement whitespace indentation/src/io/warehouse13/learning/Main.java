26d4c49dede6957a2e1a8c160d054e8cd9606f01
davidvera13 <david.vera@9online.fr>
2024-02-25 19:45:32 +0100

    feat(java 17 master class): section 02 - lec02- statement, whitespace, indentations

---
+++ Java 17 Masterclass Start Coding in 2024/section 02 - expressions-statements-codeblock-methods/lec02 - statement whitespace indentation/src/io/warehouse13/learning/Main.java
@@ -0,0 +1,15 @@
+package io.warehouse13.learning;
+
+public class Main {
+    public static void main(String[] args) {
+        int intNumber = 50;
+        intNumber++;
+        intNumber--;
+        System.out.println("This is a test : " + intNumber);
+
+        System.out.println("This is " +
+                "another test");
+
+        System.out.println("We " + " can " + " do " + "this");
+    }
+}
