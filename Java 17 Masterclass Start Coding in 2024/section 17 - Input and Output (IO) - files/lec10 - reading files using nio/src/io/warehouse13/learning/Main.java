97b8dd58f9415b5cead846cee6ff7bf7e42f4965
davidvera13 <david.vera@9online.fr>
2024-06-09 18:56:20 +0200

    feat(java 17 master class): section 17 - lec10 - reading files using nio

---
+++ Java 17 Masterclass Start Coding in 2024/section 17 - Input and Output (IO) - files/lec10 - reading files using nio/src/io/warehouse13/learning/Main.java
@@ -0,0 +1,61 @@
+package io.warehouse13.learning;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("file.encoding"));
+		System.out.println(Charset.defaultCharset());
+
+		Path path = Path.of("fixedWidth.txt");
+		try {
+			System.out.println(new String(Files.readAllBytes(path)));
+			System.out.println("**************");
+			System.out.println(Files.readString(path));
+			System.out.println("**************");
+			Pattern pattern = Pattern.compile("(.{15})(.{3})(.{12})(.{8})(.{2})");
+			Set<String> values = new TreeSet<>();
+			Files.readAllLines(path).forEach(str -> {
+				if (!str.startsWith("Name")) {
+					Matcher m = pattern.matcher(str);
+					if (m.matches())
+						values.add(m.group(3).trim());
+				}
+			});
+			System.out.println(values);
+			System.out.println("**************");
+			try (var stringStream = Files.lines(path)){
+				var results = stringStream
+						.skip(1)
+						.map(pattern::matcher)
+						.filter(Matcher::matches)
+						.map(m -> m.group(3).trim())
+						.distinct()
+						.sorted()
+						.toArray();
+				System.out.println(Arrays.toString(results));
+			}
+			System.out.println("**************");
+			try (var stringStream = Files.lines(path)){
+				var results = stringStream
+						.skip(1)
+						.map(pattern::matcher)
+						.filter(Matcher::matches)
+						.collect(Collectors.groupingBy(matcher -> matcher.group(3).trim(), Collectors.counting()));
+				results.entrySet().forEach(System.out::println);
+			}
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
